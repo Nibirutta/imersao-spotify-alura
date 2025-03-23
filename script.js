6d589b84d08404849bc64a06c5d87357d539096c
Lucas <lucas.s.augusto@hotmail.com>
2025-03-23 13:56:41 -0300

    Fourth day and challenge done

---
+++ script.js
@@ -0,0 +1,41 @@
+const searchInput = document.getElementById('search-input');
+const searchList = document.getElementById('search-list');
+const currentPlaylist = document.getElementById('current-playlist');
+
+addEventListener('input', function() {
+    const artistName = searchInput.value.toLowerCase();
+    console.log(artistName);
+    if(artistName === '') {
+        currentPlaylist.classList.add('hidden');
+        searchList.classList.remove('hidden');
+
+        return;
+    }
+
+    requestApi(artistName);
+});
+
+function requestApi(artistName) {
+    const url = `http://localhost:3000/artists`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const filteredData = data.filter((artist) => artist.name.toLowerCase().includes(artistName));
+            console.log(filteredData);
+            displayArtists(filteredData);
+        })
+}
+
+function displayArtists(artists) {
+    currentPlaylist.classList.add("hidden");
+    var artistName = document.getElementById('artist-name');
+    var artistImage = document.getElementById('artist-img');
+
+    artists.forEach(element => {
+        artistName.textContent = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    searchList.classList.remove("hidden");
+}
